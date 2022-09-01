bd3c67591cc583704ee4ec2a5b1abbf76ff457f5
Lucas Norris <luke.norris22@gmail.com>
2022-09-02 10:47:30 +1200

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './App.jsx'
+
+// ReactDOM.render(<App />, document.getElementById('app'))
+
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+const app = document.getElementById('app')
+ReactDOM.render(<App />, app)
