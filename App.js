d735ae67bea903f3d4150a93eff39efd98260ce2
Lucas Norris <luke.norris22@gmail.com>
2022-09-02 14:08:03 +1200

    add styling

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import './main.css'
 
 function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
+        <div className="previous-operand">123,456</div>
         <div className="current-operand"></div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
+      <button>/</button>
       <button>1</button>
       <button>2</button>
       <button>3</button>
